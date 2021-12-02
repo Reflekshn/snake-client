5118776654afcd9433d1cbf0611279f43cebd924
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-12-02 03:07:06 +0000

    Client side code for connecting to the LHL snake clone server

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+// Establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // Set character encoding to UTF-8
+  conn.setEncoding('utf8');
+
+  // Create event listener for when data is received
+  conn.on('data', data => console.log(data));
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
