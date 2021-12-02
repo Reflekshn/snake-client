a5b9346440e298a59332ba201bf77fdac9b5f602
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-12-02 04:14:31 +0000

    Tested out the 'Move: up' command in various ways with a delay

--- client.js
+++ client.js
@@ -10,13 +10,31 @@ const connect = () => {
   // Set character encoding to UTF-8
   conn.setEncoding('utf8');
 
-  // Event listener for a succesful connection to the server
+  // Event listeners for a successful connection to the server
   conn.on('connect', () => console.log('Successfully connected to the game server'));
+  conn.on('connect', () => conn.write('Name: JBO'));
+
+  /* 
+  // Move the snake up with a small delay
+  conn.on('connect', () => {
+    setInterval( () => conn.write('Move: up'), 50);
+  });
+ */
+
+  /* 
+  conn.on('connect', () => {
+    setTimeout(() => conn.write('Move: up'), 500);
+  });
+  conn.on('connect', () => {
+    setTimeout(() => conn.write('Move: up'), 1000);
+  });
+  conn.on('connect', () => {
+    setTimeout(() => conn.write('Move: up'), 1500);
+  });
+ */
 
   // Event listener for when data is received from the server
   conn.on('data', data => console.log(data));
-
-  conn.write('Name: JBO');
 
   return conn;
 };
