5a48e0e7a263593465a8fd8c6ce9df5b91f8a838
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-12-02 06:33:37 +0000

    Added movement key commands

--- input.js
+++ input.js
@@ -1,11 +1,16 @@
+// Stores the active TCP connection object
+let connection;
+
 // Initialize user input
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
   stdin.on("data", handleUserInput);
+
+  connection = conn;
 
   return stdin;
 };
@@ -15,6 +20,17 @@ const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  }
+
+  // WASD Movement keys
+  if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 
 };
