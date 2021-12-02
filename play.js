8fa786980521dbab58004231b8be6c8c447b5331
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-12-02 03:19:49 +0000

    Moved the connection logic into its own cclient module

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require('net');
-
-// Establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // Set character encoding to UTF-8
-  conn.setEncoding('utf8');
-
-  // Create event listener for when data is received
-  conn.on('data', data => console.log(data));
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
