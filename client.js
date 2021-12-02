dba8cb3e80d4d3d033a97aeff401670efbd2f9c6
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-12-02 03:29:14 +0000

    Refactored to use ES6 shorthand syntax

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require('net');
 
 // Establishes a connection with the game server
-const connect = function () {
+const connect = () => {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
@@ -16,4 +16,6 @@ const connect = function () {
   return conn;
 };
 
-module.exports = connect;
+module.exports = {
+  connect,
+};
